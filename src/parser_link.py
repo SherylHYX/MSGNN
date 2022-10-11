2857a2b3a04a2179750e794c13b1d98103ec2557
SherylHYX <He_YX@outlook.com>
2022-10-11 10:35:52 +0000

    default K should be 2

--- src/parser_link.py
+++ src/parser_link.py
@@ -8,7 +8,7 @@ def parameter_parser():
     parser.add_argument('--year', type=int, default=2000)
     parser.add_argument('--num_classes', type=int, default=4, help='Number of classes.')
     parser.add_argument('--direction_only_task', action='store_true', help='Whether to degrade the task to consider direction only.')
-    parser.add_argument('--K', type=int, default=1)
+    parser.add_argument('--K', type=int, default=2)
     parser.add_argument('--q', type=float, default=0)
     parser.add_argument('--weight_decay', type=float, default=0.0005)
     parser.add_argument('--dropout', type=float, default=0.5)
